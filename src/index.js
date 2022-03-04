c49df724ec8c9a47ce005a01eda71b2866bb90cc
Seaborn Lee <seabornlee.cn@gmail.com>
2022-03-04 15:10:36 +0800

    Display data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import _ from 'lodash'
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
-function component() {
-  const element = document.createElement('div');
+const firebaseConfig = {
+  apiKey: "AIzaSyDs9QAsO-7dvFdYeWXiGkqD5SvBOeeLCEg",
+  authDomain: "test-firestore-95e2c.firebaseapp.com",
+  projectId: "test-firestore-95e2c",
+  storageBucket: "test-firestore-95e2c.appspot.com",
+  messagingSenderId: "305753471562",
+  appId: "1:305753471562:web:60a4d3c834c26d5e9644b3"
+};
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  return element;
-}
+const firestore = getFirestore(app);
+const tokenCollection = collection(firestore, 'tokens');
 
-document.body.appendChild(component());
+getDocs(tokenCollection).then((snapshot) => {
+  const objs = snapshot.docs.map(doc => doc.data())
+  const data = objs.map((obj) => obj.name + ": " + obj.price)
+  const div = document.createElement("div")
+  div.innerText = data.join(",");
+  document.body.appendChild(div);
+})
